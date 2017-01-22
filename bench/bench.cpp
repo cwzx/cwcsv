b395f590b1f4f407ff9c9c05e3440583ca3e5696
Chris Welshman <chriswelshman@gmail.com>
2017-01-22 13:22:50 +0000

    Fix benchmark output

--- bench/bench.cpp
+++ bench/bench.cpp
@@ -3,16 +3,16 @@
 #include <chrono>
 #include <cwcsv/csv.h>
 
-void benchmark( const std::string& data, uint32_t N ) {
+void benchmark( const std::string& data, int32_t N ) {
 	auto parser = csv::make_parser( data );
 
-	uint32_t nb_rows = 0;
-	uint32_t nb_cells = 0;
+	int64_t nb_rows = 0;
+	int64_t nb_cells = 0;
 
 	using namespace std::chrono;
 	auto beg_time = high_resolution_clock::now();
 
-	for(uint32_t i=0;i<N;++i) {
+	for(int32_t i=0;i<N;++i) {
 		nb_rows = 0;
 		nb_cells = 0;
 		for( auto&& row : parser ) {
@@ -24,21 +24,23 @@ void benchmark( const std::string& data, uint32_t N ) {
 	}
 
 	auto end_time = high_resolution_clock::now();
-
-	auto time = duration_cast<microseconds>( end_time - beg_time );
+	auto time_elapsed = (end_time - beg_time) / N;
+	auto time_us = duration_cast<microseconds>( time_elapsed );
+	auto time_s = duration_cast<duration<double>>( time_elapsed );
 
 	std::cout
 		<< "rows: " << nb_rows
 		<< "\ncells: " << nb_cells
 		<< "\ncells/row: " << (double)nb_cells/nb_rows
-		<< "\ntime: " << time.count() / N
-		<< " us/cell\n";
+		<< "\ntime: " << time_s.count()
+		<< " s\nrate: " << nb_cells / time_us.count()
+		<< " cells/us\n";
 }
 
 int main( int argc, char** argv ) {
 
 	const char* file = ( argc > 1 ) ? argv[1] : "sample.csv";
-	uint32_t N = 1000;
+	int N = ( argc > 2 ) ? atoi(argv[2]) : 1;
 
 	benchmark( csv::load_file( file ), N );
 
